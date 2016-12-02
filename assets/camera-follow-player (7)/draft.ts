1508ef025c339d80f3b73e3b821dfb80e1b6aea2
abezuska <abezuska@gmail.com>
2016-12-01 22:46:24 -0500

    updates with ben

--- assets/camera-follow-player (7)/draft.ts
+++ assets/camera-follow-player (7)/draft.ts
@@ -1,10 +1,14 @@
 class CameraFollowPlayer extends Sup.Behavior {
+  
+  
   awake() {
     
   }
 
   update() {
-    this.actor.setPosition( Sup.getActor("player").getX(), Sup.getActor("player").getY(), this.actor.getZ() );
+    let player = Sup.getActor("Player");
+    this.actor.setPosition( player.getX(), player.getY(), this.actor.getZ() );
+    
   }
 }
 Sup.registerBehavior(CameraFollowPlayer);
---
+++ assets/camera-follow-player (7)/script.ts
@@ -0,0 +1,13 @@
+class CameraFollowPlayer extends Sup.Behavior {
+  
+  
+  awake() {
+    
+  }
+
+  update() {
+    let player = Sup.getActor("Player");
+    this.actor.setPosition( player.getX(), player.getY(), this.actor.getZ() );
+  }
+}
+Sup.registerBehavior(CameraFollowPlayer);
